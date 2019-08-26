bc61beeb606d1867770979b8839e470fec5125d7
yaoyu bao <yaobao5354@gmail.com>
2019-08-26 23:47:57 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,7 +18,6 @@ def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash| 
     if person == "Freddy Mercury"
       contact_details_hash.each do |attribute, data|
-        binding.pry
       end
     end 
   end 
